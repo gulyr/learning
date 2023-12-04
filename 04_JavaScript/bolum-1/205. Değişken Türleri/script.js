f5a0d3929fcc957da1ee6012b767fc0f5559a2dd
gulyr <115929488+gulyr@users.noreply.github.com>
2023-12-04 10:47:46 +0400

    205. Değişken Türleri

---
+++ 04_JavaScript/bolum-1/205. Değişken Türleri/script.js
@@ -0,0 +1,30 @@
+var product = 'Iphone 14 Pro' // string
+let price = 699 // number
+console.log(typeof product)
+console.log(typeof price)
+
+let number1 = '10'
+let number2 = '20'
+console.log(Number(number1) + Number(number2))
+
+let number3 = 10
+let number4 = 20
+console.log(number3.toString() + number4.toString())
+
+let name = 'aaa'
+let surname = 'bbb'
+console.log(name + ' ' + surname)
+
+let result = 45
+let goodOrBad = result >= 50
+console.log(goodOrBad)
+console.log(typeof goodOrBad) // boolean
+
+let age1
+console.log(age1)
+console.log(typeof age1)
+// yas == undefined
+
+let age2 = ''
+console.log(age2)
+console.log(typeof age2)
