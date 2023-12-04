7fd93b13c3d4f4a2171eeb9ae28905dab260ffb7
gulyr <115929488+gulyr@users.noreply.github.com>
2023-12-04 17:24:08 +0400

    207. Operatörler

---
+++ 04_JavaScript/bolum-1/207. Operatörler/script.js
@@ -0,0 +1,30 @@
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// String Operators
+// Logical Operators
+// Bitwise Operators
+// Ternary Operators
+// Type Operators
+
+let sum, subtraction, multiplication, division, mod, increment, decrement
+let a = 10,
+  b = 20,
+  c = 30
+sum = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+mod = c % a
+increment = a++ // (ilk olaraq a-nin qiymeti increment-e beraberlesdirilir, daha sonra a-nin qiymeti 1 vahid artir)
+// increment = ++a // (ilk olaraq a-nin qiymeti 1 vahid artir, daha sonra a-nin qiymeti increment-e beraberlesdirilir)
+decrement = a--
+// decrement = --a
+
+console.log('Sum = ' + sum)
+console.log('Subtraction = ' + subtraction)
+console.log('Multiplication = ' + multiplication)
+console.log('Division = ' + division)
+console.log('Mod = ' + mod)
+console.log('Increment = ' + increment)
+// console.log('Decrement = ' + decrement)
