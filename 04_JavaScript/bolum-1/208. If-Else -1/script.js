d96ca7138fbb5923d46e7ef4c51e4e0f3d2055b8
gulyr <115929488+gulyr@users.noreply.github.com>
2023-12-07 11:48:39 +0400

    208. If / Else - 1

---
+++ 04_JavaScript/bolum-1/208. If-Else -1/script.js
@@ -0,0 +1,14 @@
+let username = 'Aaa Bbb'
+let password = '1234'
+if (username == 'Aaa Bbb') {
+  if ((password = '1234')) {
+    console.log('Logged in')
+  } else {
+    console.log('Password is wrong')
+  }
+} else {
+  console.log('Username is wrong')
+}
+// if (!isLoggedIn) {
+//   console.log('Welcome')
+// }
