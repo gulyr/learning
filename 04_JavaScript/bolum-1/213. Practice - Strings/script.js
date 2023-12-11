8b44ecc2e04a68ad2ebdcde7c63f49b2929ad424
gulyr <115929488+gulyr@users.noreply.github.com>
2023-12-11 15:28:46 +0400

    Exercise - 4 solved

--- 04_JavaScript/bolum-1/213. Practice - Strings/script.js
+++ 04_JavaScript/bolum-1/213. Practice - Strings/script.js
@@ -7,9 +7,10 @@ let kursAdi = 'Komple Web Geliştirme Kursu'
 // 2- kursAdi kaç kelimeden oluşmaktadır?
 
 // 3- url https ile mi başlıyor?
-console.log(url.startsWith('https'))
+// console.log(url.startsWith('https'))
 
 // 4- kursAdi içerisinde Eğitimi kelimesi var mı?
+console.log(kursAdi.includes('Eğitimi'))
 
 // 5- url ve kursAdi değişkenlerini kullanarak aşağıdaki string bilgiyi oluşturunuz.
 
