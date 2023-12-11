338479814c4f3d2ea63be42dd37dee27c0dedbdf
gulyr <115929488+gulyr@users.noreply.github.com>
2023-12-11 14:47:27 +0400

    213. Practice - Strings

---
+++ 04_JavaScript/bolum-1/213. Practice - Strings/script.js
@@ -0,0 +1,14 @@
+let url = 'https://www.sadikturan.com'
+let kursAdi = 'Komple Web Geliştirme Kursu'
+
+// 1- url kaç karakterlidir?
+
+// 2- kursAdi kaç kelimeden oluşmaktadır?
+
+// 3- url https ile mi başlıyor?
+
+// 4- kursAdi içerisinde Eğitimi kelimesi var mı?
+
+// 5- url ve kursAdi değişkenlerini kullanarak aşağıdaki string bilgiyi oluşturunuz.
+
+// https://www.sadikturan.com/komple-web-gelistirme-kursu
