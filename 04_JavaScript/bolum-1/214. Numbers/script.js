cf1351581ca1c8f83d04b4b851b9bddf580a3f2a
gulyr <115929488+gulyr@users.noreply.github.com>
2023-12-14 11:08:20 +0400

    214. Numbers

---
+++ 04_JavaScript/bolum-1/214. Numbers/script.js
@@ -0,0 +1,25 @@
+let result
+// result = 10
+// result = '10'
+// result = Number('10')
+// result = parseInt('10.6')
+// result = parseInt('10a')
+// result = parseInt('a10')
+// result = parseFloat('10.6')
+// result = isNaN('15')
+let number = 15.123456789
+// result = number.toPrecision(5)
+// result = number.toFixed(5)
+result = Math.round()
+result = Math.ceil()
+result = Math.floor()
+result = Math.round()
+result = Math.sqrt()
+result = Math.pow()
+result = Math.abs()
+result = Math.max()
+result = Math.min()
+result = Math.random() * 10
+
+console.log(typeof result)
+console.log(result)
