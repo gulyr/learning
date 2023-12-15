b22823a4443af2e75efa54406c60b4d45bc1484d
gulyr <115929488+gulyr@users.noreply.github.com>
2023-12-15 11:03:43 +0400

    Exercise - 2 solved

--- 04_JavaScript/bolum-1/218. Practice - Arrays/script.js
+++ 04_JavaScript/bolum-1/218. Practice - Arrays/script.js
@@ -2,13 +2,16 @@
 let fruits = ['Elma', 'Armut', 'Muz', 'Cilek']
 
 // 2- Dizi kaç elemanlıdır?
+let result1 = fruits.length
+console.log(result1)
+
 // 3- Dizinin ilk ve son elemanı nedir?
 // 4- Elma dizinin bir elemanımıdır?
 // 5- Kiraz meyvesini listenin sonuna ekleyiniz.
 // 6- Dizinin son 2 elemanını siliniz.
 // 7- Aşağıdaki bilgileri dizi içerisinde saklayınız ve her öğrencinin yaşını ve not ortalamasını hesaplayınız.
 /*
-    Öğrenci 1: Yiğit Bilgi 2010 (70,60,80)
-    Öğrenci 2: Ada Bilgi 2012   (80,80,90)
-    Öğrenci 3: Ahmet  2009 (60,60,70)
-   */
+        Öğrenci 1: Yiğit Bilgi 2010 (70,60,80)
+        Öğrenci 2: Ada Bilgi 2012   (80,80,90)
+        Öğrenci 3: Ahmet  2009 (60,60,70)
+       */
