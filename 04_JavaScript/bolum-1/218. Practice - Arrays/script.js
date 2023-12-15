56c1a94e233018b9d3075fca1375f2e6522d8a47
gulyr <115929488+gulyr@users.noreply.github.com>
2023-12-15 11:57:31 +0400

    Exercise - 7 (7.2) solved

--- 04_JavaScript/bolum-1/218. Practice - Arrays/script.js
+++ 04_JavaScript/bolum-1/218. Practice - Arrays/script.js
@@ -43,3 +43,16 @@ let age_student3 = new Date().getFullYear() - student3[2]
 console.log(
   `age_student1: ${age_student1}; age_student2: ${age_student2}; age_student3: ${age_student3}`
 )
+
+// Her öğrencinin not ortalaması
+let avg_student1 = (student1[3][0] + student1[3][1] + student1[3][2]) / 3
+let avg_student2 = (student2[3][0] + student2[3][1] + student2[3][2]) / 3
+let avg_student3 = (student3[3][0] + student3[3][1] + student3[3][2]) / 3
+
+console.log(
+  `avg_student1: ${avg_student1.toFixed(
+    1
+  )}; avg_student2: ${avg_student2.toFixed(
+    1
+  )}; avg_student3: ${avg_student3.toFixed(1)}`
+)
