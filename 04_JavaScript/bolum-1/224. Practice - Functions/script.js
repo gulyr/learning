c5deb54b53923ca2cfcfa809c33fa4703a8157ef
gulyr <115929488+gulyr@users.noreply.github.com>
2023-12-28 17:00:57 +0400

    Task-5 done

--- 04_JavaScript/bolum-1/224. Practice - Functions/script.js
+++ 04_JavaScript/bolum-1/224. Practice - Functions/script.js
@@ -18,16 +18,26 @@
 // console.log(`Task-2 result: ${result}`)
 
 // 3- Yazı tura uygulamasını fonksiyon kullanarak yapınız.
+
 // 4- Kendisine gönderilen bir sayının tam bölenlerini dizi şeklinde döndüren fonksiyonu yazınız.
-function convertnumbertoarray(number) {
-  let array = []
-  for (let index = 2; index < number; index++) {
-    if (number % index == 0) {
-      array.push(index)
-    }
-  }
-  return array
-}
-console.log('Task-4 result:')
-console.log(convertnumbertoarray(50))
+// function convertnumbertoarray(number) {
+//   let array = []
+//   for (let index = 2; index < number; index++) {
+//     if (number % index == 0) {
+//       array.push(index)
+//     }
+//   }
+//   return array
+// }
+// console.log('Task-4 result:')
+// console.log(convertnumbertoarray(50))
+
 // 5- Değişken sayıda parametre alan toplam isminde bir fonksiyon tanımlayınız.
+function toplam() {
+  let result = 0
+  for (let i = 0; i < arguments.length; i++) {
+    result += arguments[i]
+  }
+  return result
+}
+console.log(toplam(2, 5, 6, 7))
