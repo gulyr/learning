1d50043151005e92a05f3d221e2a1898befe5525
gulyr <115929488+gulyr@users.noreply.github.com>
2023-12-28 15:15:39 +0400

    Task-2 done

--- 04_JavaScript/bolum-1/224. Practice - Functions/script.js
+++ 04_JavaScript/bolum-1/224. Practice - Functions/script.js
@@ -1,13 +1,22 @@
 // 1- Kendisine gönderilen kelimeyi belirtilen kez ekranda yazan fonksiyonu yapınız.
-function writeword(word, count) {
-  for (let index = 0; index < count; index++) {
-    console.log(word)
-  }
-}
-console.log('Task-1 result:')
-writeword('Aaaa', 5)
+// function writeword(word, count) {
+//   for (let index = 0; index < count; index++) {
+//     console.log(word)
+//   }
+// }
+// console.log('Task-1 result:')
+// writeword('Aaaa', 5)
 
 // 2- Dikdörtgenin alan ve çevresini hesaplayan fonksiyonu yazınız.
+function findperimeterandarea(a, b) {
+  let perimeter, area
+  perimeter = 2 * (a + b)
+  area = a * b
+  return `Perimeter = ${perimeter}; Area = ${area}`
+}
+let result = findperimeterandarea(4, 5)
+console.log(`Task-2 result: ${result}`)
+
 // 3- Yazı tura uygulamasını fonksiyon kullanarak yapınız.
 // 4- Kendisine gönderilen bir sayının tam bölenlerini dizi şeklinde döndüren fonksiyonu yazınız.
 // 5- Değişken sayıda parametre alan toplam isminde bir fonksiyon tanımlayınız.
