206342e2f97a7bbc5b198c30b889df73dbb6f8ed
gulyr <115929488+gulyr@users.noreply.github.com>
2023-12-28 15:03:30 +0400

    Task-1 done

--- 04_JavaScript/bolum-1/224. Practice - Functions/script.js
+++ 04_JavaScript/bolum-1/224. Practice - Functions/script.js
@@ -1,4 +1,11 @@
 // 1- Kendisine gönderilen kelimeyi belirtilen kez ekranda yazan fonksiyonu yapınız.
+function writeword(word, count) {
+  for (let index = 0; index < count; index++) {
+    console.log(word)
+  }
+}
+writeword('Aaaa', 5)
+
 // 2- Dikdörtgenin alan ve çevresini hesaplayan fonksiyonu yazınız.
 // 3- Yazı tura uygulamasını fonksiyon kullanarak yapınız.
 // 4- Kendisine gönderilen bir sayının tam bölenlerini dizi şeklinde döndüren fonksiyonu yazınız.
