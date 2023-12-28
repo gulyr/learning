7c080958979a61637e804f97d9b4c8b8e652192f
gulyr <115929488+gulyr@users.noreply.github.com>
2023-12-28 15:47:18 +0400

    Task-4 done

--- 04_JavaScript/bolum-1/224. Practice - Functions/script.js
+++ 04_JavaScript/bolum-1/224. Practice - Functions/script.js
@@ -8,15 +8,26 @@
 // writeword('Aaaa', 5)
 
 // 2- Dikdörtgenin alan ve çevresini hesaplayan fonksiyonu yazınız.
-function findperimeterandarea(a, b) {
-  let perimeter, area
-  perimeter = 2 * (a + b)
-  area = a * b
-  return `Perimeter = ${perimeter}; Area = ${area}`
-}
-let result = findperimeterandarea(4, 5)
-console.log(`Task-2 result: ${result}`)
+// function findperimeterandarea(a, b) {
+//   let perimeter, area
+//   perimeter = 2 * (a + b)
+//   area = a * b
+//   return `Perimeter = ${perimeter}; Area = ${area}`
+// }
+// let result = findperimeterandarea(4, 5)
+// console.log(`Task-2 result: ${result}`)
 
 // 3- Yazı tura uygulamasını fonksiyon kullanarak yapınız.
 // 4- Kendisine gönderilen bir sayının tam bölenlerini dizi şeklinde döndüren fonksiyonu yazınız.
+function convertnumbertoarray(number) {
+  let array = []
+  for (let index = 2; index < number; index++) {
+    if (number % index == 0) {
+      array.push(index)
+    }
+  }
+  return array
+}
+console.log('Task-4 result:')
+console.log(convertnumbertoarray(50))
 // 5- Değişken sayıda parametre alan toplam isminde bir fonksiyon tanımlayınız.
